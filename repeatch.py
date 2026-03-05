fc06e974809bde70fec951db78be66ace85c5f95
Deepankar <deepankar.gautam1012@gmail.com>
2026-03-05 23:54:29 +0530

    commit

---
+++ repeatch.py
@@ -0,0 +1,15 @@
+l = [1,2,2,3,4,4,4,5,5,6]
+dict = {}
+
+for i in range (0, len(l)):
+    if l[i] in dict:
+        dict[l[i]]+= 1
+    else:
+        dict[l[i]] = 1
+
+print (dict)
+
+for i in dict:
+    a = dict[i])
+
+print (f"Repeating Characters Found : ")
