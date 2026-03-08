eb8730321d0c131aa2e229b2cdb35904d8ba8ccd
Deepankar <deepankar.gautam1012@gmail.com>
2026-03-08 23:58:30 +0530

    class work

--- beta.py
+++ beta.py
@@ -12,3 +12,24 @@ for ch in password:
         empty_list.append(ch)
 
 print (f"repeated characters : {occurance_list}")
+# ----------------------------------------------------------------------------
+
+# Problem 1 : Does password contains any repeating character(s)?
+# Method 1
+
+password = input("Enter your password : ")
+
+checking_list = []
+occurrence_list = []
+
+for ch in password:
+    if ch in checking_list:
+        occurrence_list.append(ch)
+    else:
+        checking_list.append(ch)
+
+if len (occurrence_list) == 0:
+    print (f"Your password has no repeating characters, great job")
+else:
+    print (f"Your password has following repeating characters : {", ".join(occurrence_list)}" )
+
