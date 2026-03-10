b78ac9ac9a1bd34cecad2ec4332d12d14a657eb7
Deepankar <deepankar.gautam1012@gmail.com>
2026-03-10 23:48:14 +0530

    class work

--- beta.py
+++ beta.py
@@ -1,47 +1,48 @@
 # Problem 1 : Does password contains any repeating character(s)?
 # Method 1
 
-password = input("Enter your password : ")
+# password = input("Enter your password : ")
 
-checking_list = []
-occurrence_list = []
+# checking_list = []
+# occurrence_list = []
 
-for ch in password:
-    if ch in checking_list:
-        occurrence_list.append(ch)
-    else:
-        checking_list.append(ch)
+# for ch in password:
+#     if ch in checking_list:
+#         occurrence_list.append(ch)
+#     else:
+#         checking_list.append(ch)
 
-if len (occurrence_list) == 0:
-    print (f"Your password has no repeating characters, great job")
-else:
-    print (f"Your password has following repeating characters : {", ".join(occurrence_list)}" )
+# if len (occurrence_list) == 0:
+#     print (f"Your password has no repeating characters, great job")
+# else:
+#     print (f"Your password has following repeating characters : {", ".join(occurrence_list)}" )
 
 # -------------------------------------------------------------------------------------
 
 # Method 2
 
-password = input("Enter your password : ")
+# password = input("Enter your password : ")
 
-dictionary = {}
-repeated = "".split(",")
-found = False
+# dictionary = {}
+# repeated = ""
+# found = False
 
-for ch in range (len(password)):
-    if ch in dictionary:
-        dictionary[ch] += 1
-    else:
-        dictionary[ch] = 1
+# for ch in password:
+#     if ch in dictionary:
+#         dictionary[ch] += 1
+#     else:
+#         dictionary[ch] = 1
 
-for ch in dictionary:
-    if dictionary[ch] > 1:
-        found = True
-        repeated = repeated + dictionary[ch] + ","
+# for ch in dictionary:
+#     if dictionary[ch] > 1:
+#         found = True
+#         repeated = repeated + ch + ", "
 
-print(f"Your password has following repeating characters : {repeated}")
+# if found == False:
+#     print (f"Your password has no repeating characters, great job")
+# else:
+#     print(f"Your password has following repeating characters : {repeated}")
 
-if found == False:
-    print (f"Your password has no repeating characters, great job")
 
 # -------------------------------------------------------------------------------------
 
